726e26e25b29784420eaf8c582e783c963c1e414
joanl01 <a1825938@adelaide.edu.au>
2024-11-17 23:06:57 +1030

    Change data_filtered to data

--- R-code/02-PERMANOVA.R
+++ R-code/02-PERMANOVA.R
@@ -1,23 +1,23 @@
 # Load required packages 
 pacman::p_load(readxl, tidyverse, vegan, PERMANOVA)
 # Read in dataset
-data_filtered<- read_excel(here::here("datasets", "JJ_DATA.xlsx"))
+data<- read_excel(here::here("datasets", "JJ_DATA.xlsx"))
 # Remove the empty spaces
-data_filtered<- data_filtered |> na.omit()
+data<- data |> na.omit()
 
 # Select only the columns related to species abundance (P1 to P20)
-species_data <- data_filtered[, 3:22]  # Columns P1 to P20 should be species abundance columns
+species_data <- data[, 3:22]  # Columns P1 to P20 should be species abundance columns
 
 # Remove rows where all species abundances are zero
 species_data <- species_data[rowSums(species_data) > 0, ]
 
 # Filter the original `data` dataframe to match the rows of `species_data`
-data_filtered <- data_filtered[rowSums(data_filtered[, 3:22]) > 0, ] 
+data <- data[rowSums(data[, 3:22]) > 0, ] 
 
 # Check for missing values (NAs) and remove them
 if(any(is.na(species_data))) {
   species_data <- na.omit(species_data)  # Remove rows with missing values
-  data_filtered <- na.omit(data_filtered)  # Also remove corresponding rows from `data_filtered`
+  data <- na.omit(data)  # Also remove corresponding rows from `data`
 }
 
 
@@ -25,24 +25,24 @@ if(any(is.na(species_data))) {
 species_data <- species_data |> as.matrix()
 
 # Create a factor for Sampling Group and Terrain Type interaction
-group_factor <- interaction(data_filtered$`Sampling Group`, data_filtered$`Terrain Type`)
+group_factor <- interaction(data$`Sampling Group`, data$`Terrain Type`)
 
 # Inspect the group factor
 # table(group_factor)
 
 
 # Perform PERMANOVA to test the effect of Sampling Group and Terrain Type
-permanova_results <- adonis2(species_data ~ `Sampling Group` * `Terrain Type`, data = data_filtered, permutations = 999)
+permanova_results <- adonis2(species_data ~ `Sampling Group` * `Terrain Type`, data = data, permutations = 999)
 
 # Print the results
 print(permanova_results)
 
 # Test the effect of Sampling Group
-permanova_group <- adonis2(species_data ~ `Sampling Group`, data = data_filtered, permutations = 999)
+permanova_group <- adonis2(species_data ~ `Sampling Group`, data = data, permutations = 999)
 print(permanova_group)
 
 # Test the effect of Terrain Type
-permanova_terrain <- adonis2(species_data ~ `Terrain Type`, data = data_filtered, permutations = 999)
+permanova_terrain <- adonis2(species_data ~ `Terrain Type`, data = data, permutations = 999)
 print(permanova_terrain)
 
 
